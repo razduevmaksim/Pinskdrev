cb87d1405986a25ea4e1b21ba0b91ee8225bef21
Maksim Razduyeu <mrazduev@gmail.com>
2022-12-13 20:24:21 +0300

    Pinskdev

---
+++ app/src/main/java/com/maksapp/pinskdrev/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.maksapp.pinskdrev
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.maksapp.pinskdrev.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_catalog, R.id.navigation_orders, R.id.navigation_account
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        supportActionBar?.hide()
+    }
+}
